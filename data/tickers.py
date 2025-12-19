9a6360faba0e5cae1419ccfb0c86fd90d07f9788
Sharesth <sharesth444yt@gmail.com>
2025-12-19 18:04:36 +0530

    written data code in file

---
+++ data/tickers.py
@@ -0,0 +1,17 @@
+ASSETS = {
+    # Equities
+    "SP500": "^GSPC",
+    "NASDAQ": "^IXIC",
+    "DAX": "^GDAXI",
+
+    # FX
+    "EURUSD": "EURUSD=X",
+    "USDJPY": "JPY=X",
+
+    # Rates
+    "US10Y": "^TNX",
+
+    # Commodities
+    "GOLD": "GC=F",
+    "OIL": "CL=F"
+}
